6b1385e7141851554c3f468b583fb9b63db8797e
Sean Cady <seanzx85@users.noreply.github.com>
2017-06-12 11:45:21 -0400

    Material UI loading on dark theme

---
+++ public/components/landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Landing extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome</h1>
+                <div>What do you want to do?</div>
+            </div>
+        );
+    }
+}
--- public/main.js
+++ public/main.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './main.scss';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { WeightConversion } from  './components/weight-conversion.js';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+import {WeightConversion} from  './components/weight-conversion';
+import {Landing} from  './components/landing';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin()
+injectTapEventPlugin();
 
 const App = () => (
-    <MuiThemeProvider>
-        <WeightConversion/>
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <AppBar title="Gym Utilities"/>
     </MuiThemeProvider>
 );
 
-ReactDOM.render(<App/>,document.getElementById('content'));
+ReactDOM.render(<App/>, document.getElementById('application'));
 
+
+//Tabata Timer
+//Weight Percentages, raw and nearest 5 pounds
+//What should be on the bar
+//Lbs to Kilo
